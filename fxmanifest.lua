88c6590b5035a69f93c11cf6a91ab4f52d3ec5c7
Tamas Mohos <zerosuxx@gmail.com>
2020-08-15 02:42:26 +0200

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'bodacious'
+games { 'gta5' }
+resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9-d386-4d18-afbe-5e627f4af937"
+loadscreen_manual_shutdown "yes"
+client_script "client.lua"
